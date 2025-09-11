92922389af2f9e471070d0c7358b1ea8c0125006
Pankaj Kumar <pkumar@questionlab.com>
2025-09-12 01:03:41 +0530

    changessss

--- predictcpi/views/emailreader.py
+++ predictcpi/views/emailreader.py
@@ -7,11 +7,6 @@ from django.utils.decorators import method_decorator
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
-
-
-
-
-
 import msal 
 from dotenv import load_dotenv
 import re
